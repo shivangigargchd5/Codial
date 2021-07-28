da31b399e52ea6385b95cbb5d1fd2399ada4d0fd
Shivangi Garg <shivangigargchd5@gmail.com>
2021-07-28 12:29:35 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.listen(port,((err)=>{
+    if(err){
+        console.log(`Error while runnning the server: ${err}`);
+    }
+    console.log(`The server is running successfully on port: ${port}`);
+}));
